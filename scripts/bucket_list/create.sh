2fd62c92633017e41ee517794501c25b461a3840
jago425 <rmjagodowski@gmail.com>
2017-10-20 10:48:21 -0400

    add scripts for show curl request
    tested with curl and in browser...looks good!

--- scripts/bucket_list/create.sh
+++ scripts/bucket_list/create.sh
@@ -2,7 +2,9 @@ curl --include --request POST "http://localhost:4741/list_items" \
 --header "Content_Type: application/json" \
 --data '{
   "list_item": {
-    "list_description": "'"${LIST_DESCRIPTION}"'",
+    "item_description": "'"${ITEM_DESCRIPTION}"'",
     "done": "'"${DONE}"'"
   }
 }'
+
+echo
